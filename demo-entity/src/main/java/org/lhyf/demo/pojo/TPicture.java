f0e30b0f75a68121686b1b7612cc8fc88c70d3c6
lhyf <lhyf@outlook.com>
2018-06-30 08:37:31 +0800

    添加 inputfile 相关资源文件

---
+++ demo-entity/src/main/java/org/lhyf/demo/pojo/TPicture.java
@@ -0,0 +1,65 @@
+package org.lhyf.demo.pojo;
+
+import java.util.Date;
+
+public class TPicture {
+    private Integer id;
+
+    private String title;
+
+    private String url;
+
+    private Date addTime;
+
+    private String location;
+
+    private String intro;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title == null ? null : title.trim();
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url == null ? null : url.trim();
+    }
+
+    public Date getAddTime() {
+        return addTime;
+    }
+
+    public void setAddTime(Date addTime) {
+        this.addTime = addTime;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location == null ? null : location.trim();
+    }
+
+    public String getIntro() {
+        return intro;
+    }
+
+    public void setIntro(String intro) {
+        this.intro = intro == null ? null : intro.trim();
+    }
+}
